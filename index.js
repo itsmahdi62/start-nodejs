8ad5fc67c2a7ac6121f96ea364065d2069367fad
Mahdi <amiralmasi021@gmail.com>
2023-10-03 23:26:10 +0330

    Add routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const http = require('http');
+const urL= require('url');
 
 ////////////      files
 //blocking  , synchronous way
@@ -32,7 +33,19 @@ const http = require('http');
 //////////////////////////////
 // Server
 const server = http.createServer((req , res) =>{
-    res.end("Hello from the server !")
+    const pathName = req.url;
+
+    if(pathName === '/'  || pathName === '/overview'){
+        res.end("This is the OVERVIEW")
+    }else if (pathName === "/product"){
+        res.end("This is the Product")
+    }else {
+        res.writeHead(404 , {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello-world',
+        });
+        res.end('<h1>Page not found</h1>');
+    }
 })
 
 server.listen(8000 , '127.0.0.1' , () => {
