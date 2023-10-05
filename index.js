ed9356b68d5b8cf1eae54a71fb76a6fde136753c
Mahdi <amiralmasi021@gmail.com>
2023-10-05 12:26:01 +0330

    Connect html and api

--- index.js
+++ index.js
@@ -34,30 +34,50 @@ const urL= require('url');
 // Server
 //////////////
 
+const replaceTemplate = (temp , product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g , product.productName)
+    output = output.replace(/{%IMAGE%}/g , product.image)
+    output = output.replace(/{%PRICE%}/g , product.price)
+    output = output.replace(/{%FROM%}/g , product.from)
+    output = output.replace(/{%NUTRIENTS%}/g , product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g , product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g , product.description)
+    output = output.replace(/{%ID%}/g , product.id)
+
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g , 'not-organic')
+
+    return output;
+}
+
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
 
+
+
+
+
+
 const server = http.createServer((req , res) =>{
     const pathName = req.url;
-
     //overview page
     if(pathName === '/'  || pathName === '/overview'){
         res.writeHead(200 , {'Content-type' : 'text/html'});
-        res.end(tempOverview)
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard , el)).join('')
+        const output  = tempOverview.replace('{%PRODUCT_CARDS%}' , cardsHtml)
+        res.end(output)
     //product page
     }else if (pathName === "/product"){
-        res.end("This is the Product")
-
-
-    ._construct. API
+        res.writeHead(200 , {'Content-type' : 'text/html'});
+        res.end(tempOverview)
+     // API
     }else if (pathName === '/api'){
-        res.writeHead(200 , {'Content-type' : 'application/json'});
-        res.end(data)
-    
+        res.writeHead(200 , {'Content-type' : 'text/html'});
+        res.end()
         // NOT found
     }else {
         res.writeHead(404 , {
@@ -67,6 +87,8 @@ const server = http.createServer((req , res) =>{
         res.end('<h1>Page not found</h1>');
     }
 })
+
+
 
 server.listen(8000 , '127.0.0.1' , () => {
     console.log("Listening to requets")
