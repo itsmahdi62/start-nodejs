51c39a9bc9c0092b0e21fe5d90efcc2c61380875
Mahdi <amiralmasi021@gmail.com>
2023-10-08 21:39:02 +0330

    Learn how to work with streams

--- event.js
+++ event.js
@@ -1,29 +1,43 @@
-const eventEmitter = require("events");
-const { EventEmitter } = require("stream");
+// const eventEmitter = require("events");
+// const { EventEmitter } = require("stream");
 
 const http = require('http')
 
-class Sales extends EventEmitter{
-    constructor(){
-        super();
-    }
-}
+// class Sales extends EventEmitter{
+//     constructor(){
+//         super();
+//     }
+// }
 
-const myEmitter = new Sales();
+// const myEmitter = new Sales();
 
-myEmitter.on('newSale' , () => {
-    console.log(`There was a new sale! `);
-})
+// myEmitter.on('newSale' , () => {
+//     console.log(`There was a new sale! `);
+// })
 
-myEmitter.on('newSale' , stock => {
-    console.log(`Costomer name  : Jonas ${stock}`);
-})
+// myEmitter.on('newSale' , stock => {
+//     console.log(`Costomer name  : Jonas ${stock}`);
+// })
 
-myEmitter.emit('newSale' , "dd");
+// myEmitter.emit('newSale' , "dd");
 /////////////////////////
 
 const server = http.createServer();
 
 server.on('request' , (req , res ) => {
+    console.log('Request received!');
+    console.log(req.url)
+    res.end('Request received!');
+})
+
+server.on('request' , (req , res ) => {
+    console.log('another Request !');
+})
+server.on('close' , () => {
+    console.log('server closed');
     
+})
+
+server.listen(8000 , '127.0.0.1' , () =>{
+    console.log("Wating for request");
 })
